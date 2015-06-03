487cb029dfd73ce69edb897fdc19d9aae67cd337
Alexandr Kaznacheiskiy <alien000@mail.ru>
2015-06-03 16:07:58 +0600

    add logic to factory

--- app/api/api-customer.js
+++ app/api/api-customer.js
@@ -1,76 +1,76 @@
 angular.module('api-customer-application', []).factory('apiCustomer', function(){
 
-// var met = new Object();
-// met.prototype.addCustomer = function(a){
-// 				console.log(this);
-// 			};
-	// 	var m = this;
-	// 	return {
-	// 		getDataForKey: function(key){
-	// 			return m.obj[key];
-	// 		},
+	function getCustomerForEdit(customers){
+		this.selectedCustomer = customers;
+		return angular.copy(customers.customer);
+	}	
 
-	// 		getData: function(){
-	// 			return m.obj;
-	// 		},
-
-	// 		addData: function(object){
-	// 			for(key in object){
-	// 				console.log(key, object);
-	// 				m.obj[key] = object[key];
-	// 			}
-	// 		},
-
-	// 		removeCustomer: function(customer){
-	// 			m.obj.customer.splice(customer, 1);
-	// 		},
-
-
-	// 		addCustomer: function(){
-	// 			var index = m.obj.customer.push({
-	// 				name: '',
-	// 				id: new Date().getTime()
-	// 			});
-	// 			console.log(1);
-	// 			return m.obj.customer[index-1];
-	// 		}
-	// 	};
-	// };
-	//
-
-
-	function addCustomer (){
-		var index = this.obj.customer.push({
-			name: '',
-			id: new Date().getTime()
-		});
-		return this.obj.customer[index-1];
+	function saveCustomer(customer){
+		if (!this.selectedCustomer) return;
+		this.selectedCustomer.customer = angular.copy(customer);
 	}
 
-	function removeCustomer (customer){
-		this.obj.customer.splice(customer, 1);
+	function addCustomer(people){
+		if (typeof people === 'undefined') return;
+		people.id = new Date().getTime();
+		var customer = {
+			customer: angular.copy(people),
+			addOrder: addOrder,
+			removeOrder: removeOrder
+		}
+		var index = this.customers.push(customer);
+		return this.customers[index-1];
+	}
+
+
+	function removeCustomer(customer){
+		this.customers.splice(customer, 1);
+	}
+
+	function addOrder(product){
+		if (typeof product === 'undefined') return;
+
+		var index = this.customer.order.push({
+			id: new Date().getTime(),
+			name: product.name
+		});
+		return this.customer.order[index-1];
+			
+		// this.customer.push({
+		// 	id:new Date().getTime(),
+		// 	name:'qqq'
+		// });
+	}
+
+	function removeOrder(order){
+		if (typeof order === 'undefined') return;
+		this.customer.order.splice(customer, 1);
 	}
 
 	var data = [{
 		id: '1',
-		customer: []
+		firstName: 'test1'
 	}, {
 		id: '2',
-		customer: []
+		firstName: 'test2'
 	}];
 
-	var nData = [];
-	data.forEach(function(_d){
-		nData.push({
-			obj: _d,
-			addCustomer: addCustomer,
-			removeCustomer: removeCustomer
-		});
-	});
+	var organisation = {
+		addCustomer: addCustomer,
+		removeCustomer: removeCustomer,
+		getCustomerForEdit: getCustomerForEdit,
+		saveCustomer: saveCustomer,
+		customers: []
+	};
 
+	data.forEach(function(_d){
+		organisation.addCustomer(_d);
+		// _d.addCustomer=addCustomer;
+		// _d.removeCustomer=removeCustomer;
+	});
 	return {
-		getData: function(){
-			return nData;
+		getOrganisation: function(){
+			return organisation;
 		}
 	};
 });
--- app/main-application.js
+++ app/main-application.js
@@ -1,9 +1,69 @@
 angular.module('main-application').directive('mainPage', function(apiCustomer){
     return {
-        //scope: true,
+        scope: true,
         templateUrl: 'app/main-application.html',
-        link: function($scope){
-            $scope.data = apiCustomer.getData();
+        link: function($scope, el, at, mod){
+            $scope.organization = apiCustomer.getOrganisation();
         }
     };
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.directive('checkeBox', function(apiCustomer){
+    return {
+        // scope: {
+        // 	ngM: '=ngModal'
+        // },
+        require: '^ngModel',
+        template: '<div class="btn" ng-click="checked = !checked"><span ng-show="checked">X</span><span ng-hide="checked">V</span></div>',
+        // template: '<input value="{{$scope.ngM}}" type="text"/>',
+        link: function($scope, el, at, ngModel){
+
+
+ngModel.$render = function(){
+	$scope.checked = ngModel.$viewValue;
+}
+
+$scope.$watch('checked', function(){
+	ngModel.$setViewValue($scope.checked);
 });
+
+
+
+        }
+    };
+}).directive('', ['', function(){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {
+			
+		}
+	};
+}]);
